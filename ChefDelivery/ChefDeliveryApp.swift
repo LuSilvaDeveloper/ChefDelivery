80a51a90ae735969fd220f3c660ec2f91be52a05
Lucas <lusilva.dev@icloud.com>
2025-09-26 16:25:40 -0700

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Lucas Assuncao da Silva on 2025-09-26.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDeliveryTests/ChefDeliveryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryTests.swift
+//  ChefDeliveryTests
+//
+//  Created by Lucas Assuncao da Silva on 2025-09-26.
+//
+
+import Testing
+@testable import ChefDelivery
+
+struct ChefDeliveryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
